f87fe33e9577ab770b58f1614921f1201cd639e8
Jed Jocson <jed.jocson@obf.ateneo.edu>
2023-01-19 12:50:31 +0800

    First commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-5xl underline">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
